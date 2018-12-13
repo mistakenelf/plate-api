398674af03bc9f8e2be5f94d1c344737805f006c
alexknipfer <alexanderknipfer@gmail.com>
2018-12-12 22:12:51 -0600

    Initiate db connection

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"github.com/knipferrc/plate-api/handlers"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
+	"github.com/knipferrc/plate-api/handlers"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	mgo "gopkg.in/mgo.v2"
 )
 
 func main() {
 	e := echo.New()
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	databaseName := os.Getenv("DB_NAME")
+	databaseUsername := os.Getenv("DB_USERNAME")
+	databasePassword := os.Getenv("DB_PASSWORD")
 
 	// Middleware
 	e.Use(middleware.Secure())
@@ -21,8 +38,28 @@ func main() {
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 	}))
 
+	dialInfo := &mgo.DialInfo{
+		Addrs:    []string{fmt.Sprintf("%s.documents.azure.com:10255", databaseName)},
+		Timeout:  60 * time.Second,
+		Database: databaseName,
+		Username: databaseUsername,
+		Password: databasePassword,
+		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+			return tls.Dial("tcp", addr.String(), &tls.Config{})
+		},
+	}
+
+	db, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMode(mgo.Monotonic, true)
+
+	h := &handlers.Handler{DBSession: db}
+
 	// Public routes
 	e.POST("/api/login", handlers.Login)
+	e.POST("/api/addTodo", h.AddTodo)
 
 	// Restricted routes
 	r := e.Group("/api")
